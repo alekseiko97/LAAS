94988084b7daecfc7615a03a86ec1be638875e0a
Aleksei Moskaljuk <alekseiko@gmail.com>
2022-04-26 12:06:38 +0200

    Initial commit

---
+++ src/test/java/com/alekseiko/laas/LaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alekseiko.laas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
